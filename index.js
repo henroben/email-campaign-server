a34bf4ed3901ad033d580ede81a5f67d8cc79d57
Ben Mercer <ben@henroben.co.uk>
2017-08-06 19:24:50 +0100

    Phase 1 authentication done, passport & google strategy added

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express');
-const http = require('http')
+const http = require('http');
+const passport = require('passport'); // base passport
+const googleStrategy = require('passport-google-oauth20').Strategy; // strategy for google
+const keys = require('./config/keys');
+
 const app = express();
+
+// Setup Passport
+passport.use(
+	new googleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+        (accessToken, refreshToken, profile, done) => {
+			'use strict';
+			console.log('access token: ', accessToken);
+			console.log('refresh token: ', refreshToken);
+			console.log('profile: ', profile.displayName);
+			console.log('done: ', done);
+		}
+	)
+);
 
 // Routes Setup
 
-app.get('/', (req, res) => {
-	res.send({
-		hi: 'there'
-	});
-});
+app.get(
+	'/auth/google/',
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // Server Setup
 
