4178195c808dd0be09ea4792f24dbf367abb2d7d
Ben Mercer <ben@henroben.co.uk>
2017-10-08 15:48:20 +0100

    Express and passport set to use cookie based session management

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const http = require('http');
 
 const keys = require('./config/keys');
 
-mongoose.connect(
-	keys.mongoURI
-);
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+// Tell Express to use cookies
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000, // set to 30 days
+		keys: [keys.cookieKey] // key to encrypt cookie, can use multiple keys in array for extra security
+	})
+);
+// Tell Passport to use cookies
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Setup Models
 require('./models/User');
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,18 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+	"use strict";
+	done(null, user.id); // user.id is the mongo record id, not the googleId
+});
+
+passport.deserializeUser((id, done) => {
+	"use strict";
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
 passport.use(
 	new googleStrategy(
 		{
@@ -17,16 +29,19 @@ passport.use(
 			// Check to see if user already present in mongoDB
 			User.findOne({
 				googleId: profile.id
-			}).then((existingUser) => {
-				if(existingUser) {
+			}).then(existingUser => {
+				if (existingUser) {
 					// User present in mongoDB
-					console.log("User already present");
+					console.log('User already present');
+					done(null, existingUser);
 				} else {
 					// User not present in mongoDB
 					// Create new user with returned google id and save to mongoDB
 					new User({
 						googleId: profile.id
-					}).save();
+					})
+						.save()
+						.then(user => done(null, user));
 				}
 			});
 		}
