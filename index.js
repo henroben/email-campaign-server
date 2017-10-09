29cc9ee0071ea118b250dd8c09e9267d8e1c8bb8
Ben Mercer <ben@henroben.co.uk>
2017-10-09 20:07:13 +0100

    minor heroku changes

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ require('./services/passport');
 require('./routes/authRoutes')(app); // module exports function - can just pass in expected arg with ()
 
 // Server Setup
-
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 server.listen(port);
