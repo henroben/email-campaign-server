762c6a487141d4823cfaf04adf4ce45c473bc526
Ben Mercer <ben@henroben.co.uk>
2017-10-15 12:31:57 +0100

    Changed proxy settings in passport

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,8 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
+			callbackURL: '/auth/google/callback',
+			proxy: true
 		},
 		(accessToken, refreshToken, profile, done) => {
 			'use strict';
@@ -54,7 +55,8 @@ passport.use(
 		{
 			clientID: keys.facebookClientID,
 			clientSecret: keys.facebookClientSecret,
-			callbackURL: '/auth/facebook/callback'
+			callbackURL: '/auth/facebook/callback',
+			proxy: true
 		},
 		(accessToken, refreshToken, profile, done) => {
 			'use strict';
